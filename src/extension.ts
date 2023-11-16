4de621d5ea839eea3e9b7eefb0b289d4acf1a46b
GHOST <ghostdevbusiness@gmail.com>
2023-11-16 22:56:05 +0000

    feat: attempt to type better

--- src/extension.ts
+++ src/extension.ts
@@ -2,15 +2,17 @@ import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import { PopupMenuItem } from 'resource:///org/gnome/shell/ui/popupMenu.js';
 import { Button } from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import { gettext as t } from 'gettext';
 import GObject from 'gi://GObject';
 import St from 'gi://St';
 
 class CIndicator extends Button {
 	constructor() {
-		super(0.0, _('My Shiny Indicator'));
+		super(0.0, t('My Shiny Indicator'));
 	}
+
 	_init() {
-		super._init(0.0, _('My Shiny Indicator'));
+		super._init(0.0, t('My Shiny Indicator'));
 
 		this.add_child(
 			new St.Icon({
@@ -19,9 +21,10 @@ class CIndicator extends Button {
 			}),
 		);
 
-		let item = new PopupMenuItem(_('Show Notification'));
+		let item = new PopupMenuItem(t('Show Notification'));
+
 		item.connect('activate', () => {
-			Main.notify(_('Hello World! :)'));
+			Main.notify(t('Hello World! :)'));
 		});
 
 		this.menu.addMenuItem(item);
@@ -31,7 +34,7 @@ class CIndicator extends Button {
 const Indicator = GObject.registerClass(CIndicator);
 
 export default class HelloWorldExtension extends Extension {
-	_indicator: typeof Indicator | null = null;
+	_indicator: CIndicator | null = null;
 
 	enable() {
 		log(`enabling ${JSON.stringify(this.metadata, null, 2)}`);

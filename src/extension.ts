20a0dfe750f31e421fdd1c40b662d3af28711e7f
GHOST <ghostdevbusiness@gmail.com>
2023-11-17 00:33:03 +0000

    feat: gitpod icon and button to open new workspace

--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,58 @@
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import { PopupMenuItem } from 'resource:///org/gnome/shell/ui/popupMenu.js';
 import { Button } from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import { spawn } from 'resource:///org/gnome/shell/misc/util.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import { gettext as t } from 'gettext';
-import GObject from 'gi://GObject';
+import { gettext as _ } from 'gettext';
+import { currentPath } from './utils';
+import Gio from 'gi://Gio';
 import St from 'gi://St';
 
-class CIndicator extends Button {
+class Indicator {
+	public button: any;
+
 	constructor() {
-		super(0.0, t('My Shiny Indicator'));
-	}
+		this.button = new Button(0.0, _('Gitpod Menu'));
 
-	_init() {
-		super._init(0.0, t('My Shiny Indicator'));
-
-		this.add_child(
+		this.button.add_actor(
+			//? Add the Gitpod icon to the button in the top bar
 			new St.Icon({
-				icon_name: 'face-smile-symbolic',
+				gicon: Gio.icon_new_for_string(
+					`${currentPath()}/icons/gitpod.svg`,
+				),
 				style_class: 'system-status-icon',
 			}),
 		);
 
-		let item = new PopupMenuItem(t('Show Notification'));
+		const item = new PopupMenuItem(_('New Empty Workspace'));
 
 		item.connect('activate', () => {
-			Main.notify(t('Hello World! :)'));
+			spawn([
+				'xdg-open',
+				'https://gitpod.io/new?autostart=true#https://github.com/gitpod-io/empty',
+			]);
 		});
 
-		this.menu.addMenuItem(item);
+		this.button.menu.addMenuItem(item);
+	}
+
+	destroy() {
+		this.button?.destroy();
 	}
 }
 
-const Indicator = GObject.registerClass(CIndicator);
-
-export default class HelloWorldExtension extends Extension {
-	_indicator: CIndicator | null = null;
+export default class GitpodExtension extends Extension {
+	private indicator: Indicator | null = null;
 
 	enable() {
 		log(`enabling ${JSON.stringify(this.metadata, null, 2)}`);
-		this._indicator = new Indicator();
-		Main.panel.addToStatusArea(this.uuid, this._indicator);
+
+		this.indicator = new Indicator();
+		Main.panel.addToStatusArea(this.uuid, this.indicator.button);
 	}
 
 	disable() {
-		Main.panel.remove_child(this._indicator);
-		this._indicator?.destroy();
-		this._indicator = null;
+		Main.panel.remove_child(this.indicator.button);
+		this.indicator.destroy();
 	}
 }
---
+++ src/utils.ts
@@ -0,0 +1,4 @@
+export function currentPath() {
+	// ty https://github.com/pop-os/shell/blob/aafc9458a47a68c396933c637de00421f5198a2a/src/paths.ts#L2
+	return import.meta.url.split('://')[1].split('/').slice(0, -1).join('/');
+}
